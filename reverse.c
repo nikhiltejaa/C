d860e83212f393f8853c21864032a4f45f0e677d
A. Nikhil Teja <62937562+nikhiltejaa@users.noreply.github.com>
2020-08-31 14:59:09 +0530

    Add files via upload

---
+++ reverse.c
@@ -0,0 +1,14 @@
+
+// A simple C++ program to reverse string using constructor
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+
+    string str = "GeeksforGeeks";
+
+    //Use of reverse iterators
+    string rev = string(str.rbegin(),str.rend());
+
+    cout<<rev<<endl;
+    return 0;
+}
